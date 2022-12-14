b2767c7d8f9f2a32483667506674c3a12a9ab7ac
marcossnikel <marcosnikeldev@gmail.com>
2022-12-14 13:06:12 -0300

    feats

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,11 +1,18 @@
+import { useNavigate, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
-import { useParams } from "react-router-dom";
-import Carousel from "./Carrousel";
-
+import { useContext, useState } from "react";
+import Modal from "./Modal";
+import ErrorBoundary from "./ErrorBoundary";
 import fetchPet from "./fetchPet";
+import Carousel from "./Carousel";
+import AdoptedPetContext from "./AdoptedPetContext";
 
 const Details = () => {
   const { id } = useParams();
+  const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
+  // eslint-disable-next-line no-unused-vars
+  const [_, setAdoptedPet] = useContext(AdoptedPetContext);
   const results = useQuery(["details", id], fetchPet);
 
   if (results.isLoading) {
@@ -17,19 +24,42 @@ const Details = () => {
   }
 
   const pet = results.data.pets[0];
+
   return (
     <div className="details">
       <Carousel images={pet.images} />
       <div>
         <h1>{pet.name}</h1>
-        <h2>
-          {pet.animal} - {pet.breed} - {pet.city} - {pet.state}
-          <button>Adopt {pet.name}</button>
-          <p>{pet.description}</p>
-        </h2>
+        <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
+        <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+        {showModal ? (
+          <Modal>
+            <div>
+              <h1>Would you like to adopt {pet.name}?</h1>
+              <div className="buttons">
+                <button
+                  onClick={() => {
+                    setAdoptedPet(pet);
+                    navigate("/");
+                  }}
+                >
+                  Yes
+                </button>
+                <button onClick={() => setShowModal(false)}>No</button>
+              </div>
+            </div>
+          </Modal>
+        ) : null}
       </div>
     </div>
   );
 };
 
-export default Details;
+export default function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
